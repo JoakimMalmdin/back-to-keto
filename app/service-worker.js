ed079c8418aecde114851739410ecf4028685f69
ORBICAP <ORBICAP@Mac-mini.local>
2026-05-19 12:30:46 +0200

    Add app cache reset page

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = "btk-keto-v8";
+const cacheName = "btk-keto-v9";
 const assets = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
